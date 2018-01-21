1e77a62a6f22a1b67bf1d52a468f9a377ded9e7d
santhiyasanthra <31505577+santhiyasanthra@users.noreply.github.com>
2018-01-21 10:36:34 +0530

    Create powerr.c

---
+++ powerr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+int a,n,b;
+scanf("%d",&a);
+scanf("%d",&n);
+	printf("%d\t",a);
+	printf("%d\n",n);
+	
+
+if(a!=0)
+{
+	b=pow(a,n);
+	printf("%d",b);
+}
+	return 0;
+}
